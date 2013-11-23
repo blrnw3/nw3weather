e8b9ec7a54f80640ff6208d1c38e5577cff8c490
blrnw3 <b.leerodgers@nw3weather.co.uk>
2013-11-23 19:20:44 +0000

    Configured routing so incoming URLs are mapped to the correct controller, unless it's a static file, which is unmapped so loads in the normal way.

--- Site_v4/index.php
+++ Site_v4/index.php
@@ -2,10 +2,29 @@
 spl_autoload_extensions(".php");
 spl_autoload_register();
 
-use nw3\app\controller as c;
+$url = $_SERVER['REQUEST_URI'];
+$qs = $_SERVER['QUERY_STRING'];
 
-$class = 'Test';
 
-$lol = new c\Test();
+$url_parts = explode('/', $url);
+print_r ($url_parts);
+
+$controller_class = 'nw3\app\controller\\'. $url_parts[2];
+
+try {
+	class_exists($controller_class);
+} catch (LogicException $e) {
+	no_controller('bad class name '. $controller_class);
+}
+$reflection = new ReflectionClass($controller_class);
+if ($reflection->isAbstract()) {
+	no_controller('not a concrete class');
+}
+$reflection->newInstance();
+
+function no_controller($extra) {
+	echo 'No controller mapped: '. $extra;
+	die();
+}
 
 ?>
